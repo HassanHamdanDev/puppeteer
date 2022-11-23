d32648322603a5754dfb6dc81fbd68e9dec6b53c
Hassan Mohammad Hamdan <hassanhamdandev@gmail.com>
2022-11-23 18:44:12 +0300

    edit

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'hello', app: 'puppeteer' });
 });
--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -2,21 +2,23 @@ let chrome = {};
 
 const puppeteer = require('puppeteer');
 
-
-
 const getPDFhandler = async (req, res) => {
     const options = { args: ['--no-sandbox', '--disable-setuid-sandbox'], };
     const { pdftemplete } = req.body;
+    console.log(pdftemplete);
     const browser = await puppeteer.launch(options);
+
     const page = await browser.newPage();
     await page.setContent(pdftemplete);
-    const pdf = await page.pdf({
+    const buffer = await page.pdf({
         format: 'A4',
         printBackground: true,
     });
     await browser.close();
-    res.contentType('application/pdf');
-    res.send(pdf);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Length', buffer.length);
+    res.status(200).send(buffer);
 };
+
 
 module.exports = { getPDFhandler };
