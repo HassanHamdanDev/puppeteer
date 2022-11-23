e6fcf5429b9a51dfaf11a119d90410e9d9417138
Hassan Mohammad Hamdan <hassanhamdandev@gmail.com>
2022-11-23 17:01:31 +0300

    edit

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,28 +1,11 @@
 let chrome = {};
-let puppeteer;
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION == true) {
-    // running on the Vercel platform.
-    chrome = require('chrome-aws-lambda');
-    puppeteer = require('puppeteer-core');
-    console.log('chrome-aws-lambda')
-} else {
-    // running locally.
-    puppeteer = require('puppeteer');
-    console.log('puppeteer')
+const puppeteer = require('puppeteer');
 
-}
+
 
 const getPDFhandler = async (req, res) => {
-    const options = (process.env.AWS_LAMBDA_FUNCTION_VERSION == true)
-        ? {
-            args: [...chrome.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox'],
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
-        }
-        : { args: ['--no-sandbox', '--disable-setuid-sandbox'], };
+    const options = { args: ['--no-sandbox', '--disable-setuid-sandbox'], };
     const { pdftemplete } = req.body;
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
