08d1f7b6ae1f7d54f2536582d223ba6687e9d373
Hassan Mohammad Hamdan <hassanhamdandev@gmail.com>
2022-12-04 19:29:35 +0300

    edit

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -3,7 +3,9 @@ let chrome = {};
 const puppeteer = require('puppeteer');
 
 const getPDFhandler = async (req, res) => {
-    const options = { args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+    const options = {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    };
     const { pdftemplete } = req.body;
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
